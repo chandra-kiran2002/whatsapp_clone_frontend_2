e5a4a7d658721b0f55c3109eb3f2aada608767d1
Kiran <jinkachandrakiran2002@gmail.com>
2022-09-07 21:43:39 +0530

    increase in chat msg width

--- js/code.js
+++ js/code.js
@@ -49,6 +49,7 @@ var chat=[1,2,3,4,5,6,7,8,9,10]
                 }
             }
           function fun(){
+            currentTo=null;
             if(screen.width < 767){
             removestylelinks()
             create("css/persons.css")
